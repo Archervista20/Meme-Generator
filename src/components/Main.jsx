9e7e071aead6c43b25b9693f1e7d7cf0942d78b8
Archervista20 <98569429+Archervista20@users.noreply.github.com>
2023-01-12 04:56:17 +0000

    Gettingvrandom memes part

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,22 @@
+import MemesApi from '../api/MemesApi';
+
 const Main = () => {
+  const getMemesImages = () => {
+    const memesArray = MemesApi.data.memes
+    const getRandomMemes =  Math.floor(Math.random() * memesArray.length)
+    console.log(getRandomMemes)
+  }
   return (
     <div className="hero">
       <div className="hero-wrapper">
         <h1>Meme Generator</h1>
+          <div className="form">
+            <div className="form-group">
+              <input className="form-input" placeholder="Enter Title"/>
+              <input className="form-input" placeholder="Enter Text"/>
+              <button className="form-btn" onClick={getMemesImages}>Get Meme</button>
+            </div>
+          </div>
       </div>
     </div>
   )
